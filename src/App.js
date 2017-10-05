a5878372e1dd8a4559ac563189742e2e7d2791ef
Sammy Teillet <sammy.teillet@gmail.com>
2017-10-05 15:05:08 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button, Input, Checkbox } from 'semantic-ui-react'
+import HistoricGraph from './graph.js'
 
 class App extends Component {
   constructor(props) {
@@ -8,16 +9,10 @@ class App extends Component {
     this.submit = this.submit.bind(this);
     this.updateInputValue = this.updateInputValue.bind(this);
     this.state = {
-      data: [
-        {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-        {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-        {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-        {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-        {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-        {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-        {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-  ],
-      historic: [],
+      partyNumber: 0,
+      historic: [
+        {name: 'Partie: 0', Wadidou: 0, Irene: 0, BBChat:0, Cheche:0, Zig:0, Clara:0, Sammy: 0},
+      ],
       Wadidou: {
         score: 0,
         historic: [],
@@ -36,7 +31,7 @@ class App extends Component {
         victories: 0,
         newScore: 0,
       },
-      Marceau: {
+      Cheche: {
         score: 0,
         historic: [],
         victories: 0,
@@ -65,18 +60,16 @@ class App extends Component {
 
   submit() {
     var newState = this.state
-    var game = {name: 'Croute'}
+    newState.partyNumber += 1
+    var game = {name: `Partie: ${newState.partyNumber}`}
     Object.keys(newState).forEach(function(gamer) {
-      if(!['data', 'historic'].includes(gamer)) {
+      if(!['data', 'historic', 'partyNumber'].includes(gamer)) {
         newState[gamer].score += parseInt(newState[gamer].newScore)
-        console.log(game)
         game[gamer] = newState[gamer].score
-        console.log(game)
       }
     }, this);
     newState.historic.push(game);
     this.setState(newState);
-    console.log(this.state)
   }
 
   updateInputValue(evt, gamer) {
@@ -91,57 +84,79 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">UNO Score</h1>
         </header>
-
-        <div className="Gamer">
-          Wadidou <br/>
-          score: {this.state.Wadidou.score} <br/>
-          historic: {this.state.Wadidou.historic}
-          <Input value={this.state.Wadidou.newScore} onChange={evt => this.updateInputValue(evt, 'Wadidou')}></Input>
-          <Checkbox/>
+        <div className="bodytavu">
+          <div className="Graph">
+            <HistoricGraph historic={this.state.historic} />
+          </div>
+          <div className="Gamers">
+            <div className="Gamer">
+              <div className="Name">
+                Wadidou
+              </div>
+              <div className="score">
+                score: {this.state.Wadidou.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Wadidou.newScore} onChange={evt => this.updateInputValue(evt, 'Wadidou')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                Irene
+              </div>
+              <div className="score">
+                score: {this.state.Irene.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Irene.newScore} onChange={evt => this.updateInputValue(evt, 'Irene')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                BBChat
+              </div>
+              <div className="score">
+                score: {this.state.BBChat.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.BBChat.newScore} onChange={evt => this.updateInputValue(evt, 'BBChat')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                Cheche
+              </div>
+              <div className="score">
+                score: {this.state.Cheche.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Cheche.newScore} onChange={evt => this.updateInputValue(evt, 'Cheche')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                Zig
+              </div>
+              <div className="score">
+                score: {this.state.Zig.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Zig.newScore} onChange={evt => this.updateInputValue(evt, 'Zig')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                Clara
+              </div>
+              <div className="score">
+                score: {this.state.Clara.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Clara.newScore} onChange={evt => this.updateInputValue(evt, 'Clara')}></Input>
+            </div>
+            <div className="Gamer">
+              <div className="Name">
+                Sammy
+              </div>
+              <div className="score">
+                score: {this.state.Sammy.score}
+              </div>
+              <Input type={'number'} className="Input" value={this.state.Sammy.newScore} onChange={evt => this.updateInputValue(evt, 'Sammy')}></Input>
+            </div>
+            <div>
+              <Button className="SubmitButton" onClick={this.submit}>Submit</Button>
+            </div>
+          </div>        
         </div>
-        <div className="Gamer">
-          Irene <br/>
-          score: {this.state.Irene.score}
-          historic: {this.state.Irene.historic}
-          <Input value={this.state.Irene.newScore} onChange={evt => this.updateInputValue(evt, 'Irene')}></Input>
-          <Checkbox/>
-        </div>
-        <div className="Gamer">
-          BBChat <br/>
-          score: {this.state.BBChat.score}
-          historic: {this.state.BBChat.historic}
-          <Input value={this.state.BBChat.newScore} onChange={evt => this.updateInputValue(evt, 'BBChat')}></Input>
-          <Checkbox/>
-        </div>
-        <div className="Gamer">
-          Marceau <br/>
-          score: {this.state.Marceau.score}
-          historic: {this.state.Marceau.historic}
-          <Input value={this.state.Marceau.newScore} onChange={evt => this.updateInputValue(evt, 'Marceau')}></Input>
-          <Checkbox/>
-        </div>
-        <div className="Gamer">
-          Zig <br/>
-          score: {this.state.Zig.score}
-          historic: {this.state.Zig.historic}
-          <Input value={this.state.Zig.newScore} onChange={evt => this.updateInputValue(evt, 'Zig')}></Input>
-          <Checkbox/>
-        </div>
-        <div className="Gamer">
-          Clara <br/>
-          score: {this.state.Clara.score}
-          historic: {this.state.Clara.historic}
-          <Input value={this.state.Clara.newScore} onChange={evt => this.updateInputValue(evt, 'Clara')}></Input>
-          <Checkbox/>
-        </div>
-        <div className="Gamer">
-          Sammy <br/>
-          score: {this.state.Sammy.score}
-          historic: {this.state.Sammy.historic}
-          <Input value={this.state.Sammy.newScore} onChange={evt => this.updateInputValue(evt, 'Sammy')}></Input>
-          <Checkbox/>
-        </div>
-        <Button onClick={this.submit}>Submit</Button>
       </div>
     );
   }
---
+++ src/graph.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+
+class HistoricGraph extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          historic: [],
+        }
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        this.setState({historic: nextProps.historic})
+        this.forceUpdate()
+        return true
+    }
+
+    render () {
+        var historic = []
+        if (this.props.historic) {
+            historic = this.props.historic
+        }
+        console.log('render', historic)
+        return ( 
+            <LineChart width={600} height={300} data={historic} key={Math.random()}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Legend />
+                <Line type="monotone" dataKey="Wadidou" stroke="#0a0011" activeDot={{r: 8}}/>
+                <Line type="monotone" dataKey="Irene" stroke="#efe700" />
+                <Line type="monotone" dataKey="BBChat" stroke="#37bfe5" />
+                <Line type="monotone" dataKey="Cheche" stroke="#ef00d7" />
+                <Line type="monotone" dataKey="Zig" stroke="#ef0000" />
+                <Line type="monotone" dataKey="Clara" stroke="#a32ef2" />
+                <Line type="monotone" dataKey="Sammy" stroke="#82ca9d" />
+            </LineChart>
+        );
+    }
+}
+
+export default HistoricGraph;
